4a34233f1578b7b6e28c6e1b9947a87aa981ca07
iozefavichus <iozefavichus@gmail.com>
2024-02-03 11:29:17 +0200

    feat: add greetings

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const args = process.argv.slice(2);
+console.log(args);
+const usernameArg = args.find(arg => arg.startsWith('--username='));
+console.log(usernameArg);
+const username = usernameArg ? usernameArg.split('=')[1] : 'Anonymous';
+
+const greeting = () => {
+    console.log(`Welcome to the File Manager, ${username}!`);
+  }
+
+greeting();
